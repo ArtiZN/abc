7322af1a99e96e1a0a3d4b51d1a6ec84d83fe1f4
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2020-05-15 20:02:39 +0300

    fix notations

--- src/paper/data.js
+++ src/paper/data.js
@@ -1,31 +1,33 @@
 export const initialNotation = `
-  X:2710
-  T:Title 
-  C:Your Name
-  M:4/4
-  L:1/16
-  Q:1/4=130
-  K:A
+
+X:2710
+T:Title 
+C:Your Name
+M:4/4
+L:1/16
+Q:1/4=130
+K:A
   V:1 clef = treble
   A2 A2 A F3 A2 F2 E2 F2|A2 A2 A B3 A4 F4|A2 A2 A F3 A2 F2 E2 F2| F2 F2 F E3 F4 z4|
   A2 A2 A F3 A2 F2 E2 F2|A2 A2 A B3 A4 F4|A2 A2 A F3 A2 F2 E2 F2| F2 F2 F E3 F4 z4|
   A2 A2 A F3 A2 F2 E2 F2|A2 A2 A B3 A4 F4|A2 A2 A F3 A2 F2 E2 F2| F2 F2 F E3 F4 z4|
   A2 A2 A F3 A2 F2 E2 F2|A2 A2 A B3 A4 F4|A2 A2 A F3 A2 F2 E2 F2| F2 F2 F E3 F4 z4|
   V:2 clef = bass
-  z16|z16|z16|z16|
-  F,,4 F,,4 F,4 F,,4|  A,,4 A,,4 A,4 A,,4|E,,4 E,,4 E,4 E,,4|D,,4 D,4 D,,4 z4| 
-  F,,2 F,2 F,,2 F,2 F,,2 F,2 F,,2 F,2|A,,2 A,2 A,,2 A,2 A,,2 A,2 A,,2 A,2|E,,2 E,2 E,,2 E,2 E,,2 E,2 E,,2 E,2|D,,2 D,2 D,,2 D,2 D,,2 D,2 D,,2 D,2| 
-  [F,,4 F,4][F,,4 F,4][F,,4 F,4][F,,4 F,4]|[A,,4 A,4][A,,4 A,4][A,,4 A,4][A,,4 A,4]|[E,,4 E,4][E,,4 E,4][E,,4 E,4][E,,4 E,4]|[F,,2 F,2][F,,2 F,2][F,, F,][E,,3 E,3][F,,4 F,4] z4 
+ z16|z16|z16|z16|
+ F,,4 F,,4 F,4 F,,4|  A,,4 A,,4 A,4 A,,4|E,,4 E,,4 E,4 E,,4|D,,4 D,4 D,,4 z4| 
+ F,,2 F,2 F,,2 F,2 F,,2 F,2 F,,2 F,2|A,,2 A,2 A,,2 A,2 A,,2 A,2 A,,2 A,2|E,,2 E,2 E,,2 E,2 E,,2 E,2 E,,2 E,2|D,,2 D,2 D,,2 D,2 D,,2 D,2 D,,2 D,2| 
+ [F,,4 F,4][F,,4 F,4][F,,4 F,4][F,,4 F,4]|[A,,4 A,4][A,,4 A,4][A,,4 A,4][A,,4 A,4]|[E,,4 E,4][E,,4 E,4][E,,4 E,4][E,,4 E,4]|[F,,2 F,2][F,,2 F,2][F,, F,][E,,3 E,3][F,,4 F,4] z4 
+ 
 `;
 
 const ChopinWaltzABC = `
-  X:2710
-  T:waltz
-  C:Chopin
-  M:3/4
-  L:1/16
-  Q:1/4=130
-  K:Em
+X:2710
+T:waltz
+C:Chopin
+M:3/4
+L:1/16
+Q:1/4=130
+K:Em
   B,4 |E2 F2 G4 G4 |A2 B2 c8 |F2 G2 A2 e2 d2 c2 |B2 ^A2 B8 |E2 F2 G4 G4 |A2 B2 c8 |F2 G2 A2 e2 d2 F2 |G8 B,4 |
   %W:
   % voice 1 (1 lines, 30 notes)
@@ -39,145 +41,142 @@ const ChopinWaltzABC = `
 `;
 
 const BeethovenSonataABC = `
-  X:2710
-  T:Sonata N8
-  C:L.V.Beethoven
-  M:4/4
-  L:1/16
-  Q:1/4=56
-  K:Cm
-    V:1
-    !fp![E,4G,4C4]- [E,3/G,3/C3/]!3![G,/C/]!4![G,3/=B,3/D3/]!5![G,/C/E/] ([=A,4C4E4]!4![=B,2D2])z2|\
-    !fp!!3![=B,4D4F4]- [B,3/D3/F3/][B,/D/F/][B,3/D3/G3/][B,/D/A/] ([B,4D4A4]!3![C2E2G2])z2|
-    V:2
-    [C,,4E,,4G,,4C,4]- [C,,3/E,,3/G,,3/C,3/]!2!E,/!3!D,3/!4!C,/ (!2!^F,4G,2)z _A,,|\
-    _A,4-A,3/!2!A,/!1!G,3/=F,/ E,4-E,2z3/ E,/|
+X:2710
+T:Sonata N8
+C:L.V.Beethoven
+M:4/4
+L:1/16
+Q:1/4=56
+K:Cm
+  V:1
+  !fp![E,4G,4C4]- [E,3/G,3/C3/]!3![G,/C/]!4![G,3/=B,3/D3/]!5![G,/C/E/] ([=A,4C4E4]!4![=B,2D2])z2|\
+  !fp!!3![=B,4D4F4]- [B,3/D3/F3/][B,/D/F/][B,3/D3/G3/][B,/D/A/] ([B,4D4A4]!3![C2E2G2])z2|
+  V:2
+  [C,,4E,,4G,,4C,4]- [C,,3/E,,3/G,,3/C,3/]!2!E,/!3!D,3/!4!C,/ (!2!^F,4G,2)z _A,,|\
+  _A,4-A,3/!2!A,/!1!G,3/=F,/ E,4-E,2z3/ E,/|
 `;
 
 const ChopinOp28N4 = `
-  X:2710
-  T:Prelude
-  T:Op.28 no. 4
-  C:Frederic Chopin
-  M:4/4
-  L:1/8
-  Q:1/4=60
-  K:Em
-  V:1
-  B,>B|B6 c2|B6 c2|B6 c2|B6 _B2|
-  V:2
-  z2|[EB,G,][EB,G,][EB,G,][EB,G,] [EB,G,][EB,G,][EB,G,][EB,G,]|\
-  [EA,F,][EA,F,][EA,F,][EA,F,] [_EA,F,][EA,F,][EA,F,][EA,F,]|\
-  [_EA,=F,][EA,F,][EA,F,][EA,F,] [DA,F,][DA,F,][D^G,F,][DG,F,]|\
-  [D^G,E,][DG,E,][DG,E,][DG,E,] [D=G,E,][DG,E,][^CG,E,][CG,E,]|
-  %
-  V:1
-  A6 B2|A6 B2|A6 B>A|A6 ^G2-|
-  V:2
-  [=CG,E,][CG,E,][CG,E,][CG,E,] [C^F,E,][CF,E,][CF,E,][CF,E,]|\
-  [CF,E,][CF,E,][CF,E,][CF,E,] [CF,^D,][CF,^D,][CF,^D,][CF,^D,]|\
-  [CF,=D,][CF,D,][CF,D,][CF,D,] [CF,D,][CF,D,][CF,D,][CF,D,]|\
-  [C=F,D,][CF,D,][CF,D,][CF,D,] [B,F,D,][B,F,D,][B,F,D,][B,F,D,]|
-  %
-  V:1
-  ^G2 AB dcEA|F6 A2|F6 {B}A2|GFCB, ^EG(3dcB|
-  V:2
-  [B,E,C,][B,E,C,][A,E,C,][A,E,C,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
-  [A,E,B,,][A,E,B,,][A,^D,B,,][A,D,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
-  [A,^D,B,,][A,D,B,,][A,D,B,,][A,D,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
-  [A,2^D,2B,,2] z2 z4|
-  %
-  V:1
-  B6 c2|B6 c2|B6 c2|B>^A A2 g2 f>e|
-  V:2
-  [EB,G,][EB,G,][EB,G,][EB,G,] [EB,G,][EB,G,][EB,G,][EB,G,]|\
-  [EA,F,][EA,F,] [EA,F,][EA,F,] [_EA,=F,][EA,F,] [EA,F,][EA,F,]|\
-  [_E_A,=F,][EA,F,][EA,F,][EA,F,] [DA,E,][DA,E,][DA,E,][DA,E,]|\
-  [DG,E,][DG,E,][^CG,E,][^CG,E,] [^A,E,^C,][A,E,C,][=A,E,C,][A,E,C,]|
-  %
-  V:1
-  e^dc'd degB|=dB(3eE=A ^F3A|F6 {B}A2|F6- F>E|
-  V:2
-  [B,,B,,,][AFCA,][AFCA,][AFCA,] [F^DB,G,][EB,G,][EB,G,][EB,G,]|\
-  [ECA,][ECA,]A,,[CF,E,] [B,E,B,,][B,E,B,,][A,E,C,][A,E,C,]|\
-  [B,E,B,,][B,E,B,,][B,E,B,,][B,E,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
-  [B,E,B,,][B,E,B,,][B,E,B,,][B,E,B,,] [B,^D,B,,][B,D,B,,][A,D,B,,][A,D,B,,]|
-  %
-  V:1
-  E6 F2|E6 F2|E4 Hz4|[E4B,4F,4E,4] [^D4B,4F,4^D,4]|H[E8B,8G,8E,8]|]
-  V:2
-  [G,E,C,][G,E,C,][G,E,C,][G,E,C,] [_B,E,C,][B,E,C,][A,E,C,][A,E,C,]|\
-  [A,E,B,,][A,E,B,,][^G,E,B,,][G,E,B,,] [=G,E,B,,][G,E,B,,][G,E,B,,][G,E,B,,]|[G,4C,4_B,,4] Hz4|\
-  [=B,,4=B,,,4] [B,,4F,,4B,,,4]|H[E,,8E,,,8]|]
-`;
-
+X:2710
+T:Prelude
+T:Op.28 no. 4
+C:Frederic Chopin
+M:4/4
+L:1/8
+Q:1/4=60
+K:Em
+V:1
+B,>B|B6 c2|B6 c2|B6 c2|B6 _B2|
+V:2
+z2|[EB,G,][EB,G,][EB,G,][EB,G,] [EB,G,][EB,G,][EB,G,][EB,G,]|\
+[EA,F,][EA,F,][EA,F,][EA,F,] [_EA,F,][EA,F,][EA,F,][EA,F,]|\
+[_EA,=F,][EA,F,][EA,F,][EA,F,] [DA,F,][DA,F,][D^G,F,][DG,F,]|\
+[D^G,E,][DG,E,][DG,E,][DG,E,] [D=G,E,][DG,E,][^CG,E,][CG,E,]|
+%
+V:1
+A6 B2|A6 B2|A6 B>A|A6 ^G2-|
+V:2
+[=CG,E,][CG,E,][CG,E,][CG,E,] [C^F,E,][CF,E,][CF,E,][CF,E,]|\
+[CF,E,][CF,E,][CF,E,][CF,E,] [CF,^D,][CF,^D,][CF,^D,][CF,^D,]|\
+[CF,=D,][CF,D,][CF,D,][CF,D,] [CF,D,][CF,D,][CF,D,][CF,D,]|\
+[C=F,D,][CF,D,][CF,D,][CF,D,] [B,F,D,][B,F,D,][B,F,D,][B,F,D,]|
+%
+V:1
+^G2 AB dcEA|F6 A2|F6 {B}A2|GFCB, ^EG(3dcB|
+V:2
+[B,E,C,][B,E,C,][A,E,C,][A,E,C,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
+[A,E,B,,][A,E,B,,][A,^D,B,,][A,D,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
+[A,^D,B,,][A,D,B,,][A,D,B,,][A,D,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
+[A,2^D,2B,,2] z2 z4|
+%
+V:1
+B6 c2|B6 c2|B6 c2|B>^A A2 g2 f>e|
+V:2
+[EB,G,][EB,G,][EB,G,][EB,G,] [EB,G,][EB,G,][EB,G,][EB,G,]|\
+[EA,F,][EA,F,] [EA,F,][EA,F,] [_EA,=F,][EA,F,] [EA,F,][EA,F,]|\
+[_E_A,=F,][EA,F,][EA,F,][EA,F,] [DA,E,][DA,E,][DA,E,][DA,E,]|\
+[DG,E,][DG,E,][^CG,E,][^CG,E,] [^A,E,^C,][A,E,C,][=A,E,C,][A,E,C,]|
+%
+V:1
+e^dc'd degB|=dB(3eE=A ^F3A|F6 {B}A2|F6- F>E|
+V:2
+[B,,B,,,][AFCA,][AFCA,][AFCA,] [F^DB,G,][EB,G,][EB,G,][EB,G,]|\
+[ECA,][ECA,]A,,[CF,E,] [B,E,B,,][B,E,B,,][A,E,C,][A,E,C,]|\
+[B,E,B,,][B,E,B,,][B,E,B,,][B,E,B,,] [A,E,C,][A,E,C,][A,E,C,][A,E,C,]|\
+[B,E,B,,][B,E,B,,][B,E,B,,][B,E,B,,] [B,^D,B,,][B,D,B,,][A,D,B,,][A,D,B,,]|
+%
+V:1
+E6 F2|E6 F2|E4 Hz4|[E4B,4F,4E,4] [^D4B,4F,4^D,4]|H[E8B,8G,8E,8]|]
+V:2
+[G,E,C,][G,E,C,][G,E,C,][G,E,C,] [_B,E,C,][B,E,C,][A,E,C,][A,E,C,]|\
+[A,E,B,,][A,E,B,,][^G,E,B,,][G,E,B,,] [=G,E,B,,][G,E,B,,][G,E,B,,][G,E,B,,]|[G,4C,4_B,,4] Hz4|\
+[=B,,4=B,,,4] [B,,4F,,4B,,,4]|H[E,,8E,,,8]|]
+`
 const Mozart = `
-  X:1
-  T: Alphabet song
-  M:4/4
-  L:1/4
-  Q:1/4=100
-  K:D
-  |"D"D D A A|"G"B B "D"A2|"G"G G "D"F F|"A"E/2E/2E/2E/2 "D"D2|
-  w:  A B C D    E F    G     H I    J K    L  M  N  O      P
-    A A "G"G F|"D"F F "A"E2|"D"A A "G"G G|"D"F F "A"E2|
-  w:Q R    S and  T U    V     W~(dou-ble u) and  X Y    Z
-  "D"D   D    A    A|"G"B B "D"A2|"G"G    G "D"F    F|"A"E     E "D"D2|
-  w: Now I've said my   A B    C's.  Tell me   what you  think of   me.
-`;
+X:1
+T: Alphabet song
+M:4/4
+L:1/4
+Q:1/4=100
+K:D
+|"D"D D A A|"G"B B "D"A2|"G"G G "D"F F|"A"E/2E/2E/2E/2 "D"D2|
+w:  A B C D    E F    G     H I    J K    L  M  N  O      P
+  A A "G"G F|"D"F F "A"E2|"D"A A "G"G G|"D"F F "A"E2|
+w:Q R    S and  T U    V     W~(dou-ble u) and  X Y    Z
+"D"D   D    A    A|"G"B B "D"A2|"G"G    G "D"F    F|"A"E     E "D"D2|
+w: Now I've said my   A B    C's.  Tell me   what you  think of   me.
 
+`
 const SadAir = `
-  X:5
-  T:Carolan's Dream
-  R:air
-  Z:id:hn-air-5
-  M:C|
-  L:1/8
-  Q:1/2=50
-  K:Am
-  |: AB | c2 A>A A2 GA | dc A>G G2 cd | e2de dcA>A | A6 de |
-  f2gf e2fe | dcde a3a | gede dc A>A | A6 :|
-  |: ef | gega g3a | gede g2eg | agab ageg | a6 ag |
-  f2gf e2fe | dcde a3a | gede dc A>A | A6 :|
-  P:Carolan's Dream
-  |: AB | c2 B>A A2 GA | c2 BA G2 cd | e2de dcBA | A4 A2 de |
-  f2fg e2ef | dcde a3g | aged dcAG | A6 :|
-  |: ef | gede g3a | gede g3e | agab ageg | a6 ag |
-  f2fg e2ef | dcde a3g | aged dcAG | A6 :| 
-`;
-
+X:5
+T:Carolan's Dream
+R:air
+Z:id:hn-air-5
+M:C|
+L:1/8
+Q:1/2=50
+K:Am
+|: AB | c2 A>A A2 GA | dc A>G G2 cd | e2de dcA>A | A6 de |
+f2gf e2fe | dcde a3a | gede dc A>A | A6 :|
+|: ef | gega g3a | gede g2eg | agab ageg | a6 ag |
+f2gf e2fe | dcde a3a | gede dc A>A | A6 :|
+P:Carolan's Dream
+|: AB | c2 B>A A2 GA | c2 BA G2 cd | e2de dcBA | A4 A2 de |
+f2fg e2ef | dcde a3g | aged dcAG | A6 :|
+|: ef | gede g3a | gede g3e | agab ageg | a6 ag |
+f2fg e2ef | dcde a3g | aged dcAG | A6 :| 
+`
 const WaltzOfToys = `
-  X:6
-  T:Waltz of the Toys
-  R:waltz
-  C:Michel Faubert, Canada
-  Z:id:hn-waltz-6
-  M:3/4
-  K:D
-  fe fg fe|dB AF D2|fe fg fe|dB AF D2|e2 ef ed|c3 d cB|A^G AB cA|dB AF D2|
-  fe fg fe|dB AF D2|fe fg fe|dB AF D2|e2 ef ed|c3 d cB|AB AG FE|1 D3 F Ad:|2 D3 d dc||
-  dA FA dA|B3 d dB|cA Bc de|f3 f df|bf df bf|g3 e ed|ce Ac ae|f3 d dc|
-  BF FB BF|G3 e ed|cB AG FE|F4 F2|GF GA Bc|de f2 d2|ge cA Bc|1 d3 d dc:|2 d3 F Ad||
-`;
+X:6
+T:Waltz of the Toys
+R:waltz
+C:Michel Faubert, Canada
+Z:id:hn-waltz-6
+M:3/4
+K:D
+fe fg fe|dB AF D2|fe fg fe|dB AF D2|e2 ef ed|c3 d cB|A^G AB cA|dB AF D2|
+fe fg fe|dB AF D2|fe fg fe|dB AF D2|e2 ef ed|c3 d cB|AB AG FE|1 D3 F Ad:|2 D3 d dc||
+dA FA dA|B3 d dB|cA Bc de|f3 f df|bf df bf|g3 e ed|ce Ac ae|f3 d dc|
+BF FB BF|G3 e ed|cB AG FE|F4 F2|GF GA Bc|de f2 d2|ge cA Bc|1 d3 d dc:|2 d3 F Ad||
+`
 
 const ParnellsMarch = `
-  X:30
-  T:Parnell's March
-  R:march
-  M:6/8
-  L:1/8
-  Q:3/8=110
-  K:G
-  d2c | BGD BGD | d3 d2c | BGD BGD | ded d2c |
-  BcB GBd | edc BAG | ABA ded | ~A3 d2c |
-  BGD BGD | ded d2B | c2B c2e | ~g3 gfe |
-  d2B GAB | c2A F2G | ~A3 AGF | G3 :|
-  |: g2f | e3 c2G | c2e gfe | ~d3 ded | B3 B2c | 
-  d3 d^cd | ed=c BAG | ABA ded | A2d d2c | 
-  BGD BGD | ded dcB | c2B c2e | ~g3 gfe | 
-  d2B GAB | c2A F2G | ABA AGF | G3 :| 
-`;
-
+X:30
+T:Parnell's March
+R:march
+M:6/8
+L:1/8
+Q:3/8=110
+K:G
+d2c | BGD BGD | d3 d2c | BGD BGD | ded d2c |
+BcB GBd | edc BAG | ABA ded | ~A3 d2c |
+BGD BGD | ded d2B | c2B c2e | ~g3 gfe |
+d2B GAB | c2A F2G | ~A3 AGF | G3 :|
+|: g2f | e3 c2G | c2e gfe | ~d3 ded | B3 B2c | 
+d3 d^cd | ed=c BAG | ABA ded | A2d d2c | 
+BGD BGD | ded dcB | c2B c2e | ~g3 gfe | 
+d2B GAB | c2A F2G | ABA AGF | G3 :| 
+`
 export const notations = [
   {
     title: 'Chopin Waltz A minor',
